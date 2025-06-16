2c192a7313c90e8c8ba4750d48ed4a96bd7b42ea
Pamokkha <sawpamaukkha@gmail.com>
2025-06-16 08:36:32 -0500

    project frontend

---
+++ frontend/register.php
@@ -0,0 +1,101 @@
+<?php 
+include '../dbconnect.php';
+session_start();
+if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = htmlspecialchars ($_POST['userName']);
+    $email = htmlspecialchars ($_POST['userEmail']);
+    $password = htmlspecialchars ($_POST['userPassword']);
+    $confirmPassword = htmlspecialchars ($_POST['userComfirmPassword']);
+    // echo $name .','. $email .', '. $password .', '. $confirmPassword;
+
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+    $stmt->execute([
+        'email' => $email
+    ]);
+    $user = $stmt->fetch();
+    // var_dump($user);
+
+    if($password != $confirmPassword){ // password does not match
+        header('Location: register.php');
+        exit();
+    } else if (!$user) {
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        // echo "Password does not match";
+
+        $stmt = $pdo->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
+        $stmt->execute([
+            'name' => $name,
+            'email' => $email,
+            'password' => $hashedPassword
+        ]);
+        header('Location: login.php');   
+    } else {
+        header('Location: register.php');
+        exit();
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Blog Home - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Responsive navbar-->
+        <?php include 'navbar.php'; ?>
+        <!-- Page header with logo and tagline-->
+        <!-- <header class="py-5 bg-light border-bottom mb-4">
+            <div class="container">
+                <div class="text-center my-5">
+                    <h1 class="fw-bolder">Welcome to Blog Home!</h1>
+                    <p class="lead mb-0">A Bootstrap 5 starter layout for your next blog homepage</p>
+                </div>
+            </div>
+        </header> -->
+        <!-- Register page-->
+        <div class="container py-5">
+            <div class="row justify-content-md-center">
+               <div class="col-lg-6">
+                    <h3>Register </h3>
+                    <form action="#" method="post" class="p-4 p-md-5 border rounded-3 bg-light">
+                        <div class="form-floating mb-3">
+                            <input type="text" name="userName" id="floatingInputname" class="form-control" placeholder="Shwe U">
+                            <label for="floatingInputname">Name</label>
+                        </div>
+                        <div class="form-floating mb-3">
+
+                            <input type="text" name="userEmail" id="floatingInputname" class="form-control" placeholder="name@gmail">
+                            <label for="floatingInputname">Email Address</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="password" name="userPassword" id="floatingInputname" class="form-control" placeholder="Password">
+                            <label for="floatingPassword">Password</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="password" name="userComfirmPassword" id="floatingInputname" class="form-control" placeholder="Comfirm Password">
+                            <label for="floatingcomfirmPassword">Comfirm Password</label>
+                        </div>
+                        <div class="d-grid gap-2">
+                            <button type="submit" class="btn btn-primary">Register</button>
+                        </div>
+                    </form>
+               </div>
+            </div>
+        </div>
+        <!-- Footer-->
+        <?php include 'footer.php'; ?>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
+
